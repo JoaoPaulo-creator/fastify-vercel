a2a02742748b3b977bf86421fa0583b44c0ccbc7
JoaoPaulo-creator <joaosarto@gmail.com>
2023-07-17 21:18:54 -0300

    first commit

---
+++ api/serverless.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// Read the .env file.
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// Require the framework
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true,
+});
+
+// Register your application as a normal plugin.
+app.register(import("../src/app.js"));
+app.register(cors, {
+  origin: true,
+});
+
+export default async (req, res) => {
+  await app.ready();
+  app.server.emit("request", req, res);
+};
---
+++ src/server.js
@@ -0,0 +1,9 @@
+import fastify from "fastify";
+
+async function routes(fastify, options) {
+  fastify.get("/", async (request, reply) => {
+    return { hello: "world" };
+  });
+}
+
+export default routes;
